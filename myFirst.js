2d1d7e492d91d6a849d65fa333d4f2af887a498e
Thomas Batard <thomas.batard@gmail.com>
2022-01-27 07:49:10 +0100

    pour test concourse

--- myFirst.js
+++ myFirst.js
@@ -3,6 +3,7 @@ var dt = require('./myfirstmodule')
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url);
     res.write("The date and time are currently: " + dt.myDateTime());
     res.end();
 }).listen(8081);
